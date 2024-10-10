d2c459fe1f80df1b3966319b540d9684b385c2e1
Karthik Chaoji <krozzgaming9@gmail.com>
2024-10-10 18:28:40 +0530

    Delete user-model.js

--- user-model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  watchedShows: [{ type: Number }] // Store TMDB IDs
-});
-
-module.exports = mongoose.model('User', userSchema);
